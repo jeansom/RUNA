03fcad04bdcb022d7b2da00ae79c848398be0f6d
Alejandro <gomez@rutgers.physics.edu>
2015-02-04 11:12:00 -0600

    fixing bugs

--- RUNSimulation/test/crab3.py
+++ RUNSimulation/test/crab3.py
@@ -1,24 +1,23 @@
-from WMCore.Configuration import Configuration
-config = Configuration()
+from CRABClient.client_utilities import getBasicConfig
+config = getBasicConfig()
 
-config.section_("General")
-config.General.requestName = 'NAME_GENSIM_v720'
-#config.General.workArea = 'crab_projects'
+config.General.requestName = 'NAME_v720'
+config.General.workArea = 'crab_projects'
 
-config.section_("JobType")
 config.JobType.pluginName = 'PrivateMC'
 config.JobType.generator = 'lhe'
 config.JobType.psetName = 'test'
 
-config.section_("Data")
 config.Data.primaryDataset = 'NAME'
 config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = 100
 NJOBS = 1000
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.publication = True
-config.Data.publishDataName = 'NAME_GENSIM_v720'
-
-config.section_("Site")
+config.Data.publishDataName = 'GENSIM_PHYS14_v720'
 config.Site.storageSite = 'T3_US_FNALLPC'
+
+#### For hexfarm... but it does not work yet
+#config.Data.outLFN = '/store/user/algomez/data23/'	### only for hexfarm
+#config.Site.storageSite = 'T3_US_Rutgers'
 
--- RUNSimulation/test/crab3_RAW.py
+++ RUNSimulation/test/crab3_RAW.py
@@ -1,23 +1,25 @@
-from WMCore.Configuration import Configuration
-config = Configuration()
+from CRABClient.client_utilities import getBasicConfig
+config = getBasicConfig()
 
-config.section_("General")
 config.General.requestName = 'NAME'
+config.General.workArea = 'crab_projects'
 
-config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'test.py'
 
-config.section_("Data")
 config.Data.inputDataset = 'None'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 2
-NJOBS = 500
+config.Data.unitsPerJob = 1
+NJOBS = 1000
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.publication = True
-config.Data.publishDataName = 'NAME'
+config.Data.publishDataName = 'PROC_PHYS14_v720'
+config.Data.ignoreLocality = True
 
-config.section_("Site")
 config.Site.storageSite = 'T3_US_FNALLPC'
+
+#### Only for hexfarm.. it still does not work
+#config.Data.outLFN = '/store/user/algomez/data23/'	### only for hexfarm
+#config.Site.storageSite = 'T3_US_Rutgers'
 
