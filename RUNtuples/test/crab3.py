c1271bcc55ad029327c97653cc9747df91361b17
Alejandro <gomez@rutgers.physics.edu>
2015-03-22 22:57:19 -0500

    adding crab3 files

--- RUNtuples/test/crab3.py
+++ RUNtuples/test/crab3.py
@@ -1,8 +1,9 @@
 from CRABClient.UserUtilities import config
 config = config()
 
+version = 'v03'
 
-config.General.requestName = 'RPVSt100tojj_13TeV_pythia8_RUNA_PU40bx50_v01'
+config.General.requestName = ''
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
@@ -10,7 +11,7 @@ config.JobType.psetName = 'RUNtuples_cfg.py'
 #config.JobType.pyCfgParams = [ 'outputLabel=RPVSt100tojj_13TeV_pythia8_EDMNtuple_PU40bx50.root' ]
 config.JobType.allowNonProductionCMSSW = True
 
-config.Data.inputDataset = '/RPVSt100tojj_13TeV_pythia8_GENSIM/algomez-RPVSt100tojj_13TeV_pythia8_MiniAOD_v706_PU40bx50-b71e879835d2f0083a0e044b05216236/USER'
+config.Data.inputDataset = ''
 config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSReader'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
@@ -19,7 +20,21 @@ config.Data.unitsPerJob = 1
 config.Data.outLFN = '/store/user/algomez/'
 config.Data.publication = True
 config.Data.ignoreLocality = True
-config.Data.publishDataName = 'RUNA_PU40bx50_v01'
-#config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter'
+config.Data.publishDataName = 'RUNA_PHYS14_PU20bx25_'+version
 
 config.Site.storageSite = 'T3_US_FNALLPC'
+
+if __name__ == '__main__':
+
+	from CRABAPI.RawCommand import crabCommand
+
+	QCDHT = [ 
+			#'/RPVSt100tojj_13TeV_pythia8/algomez-MiniAOD_PHYS14_v720-b1b44dbfc276814daa37c582f825184d/USER',
+			#'/RPVSt100tobj_13TeV_pythia8/algomez-MiniAOD_PHYS14_v720-b1b44dbfc276814daa37c582f825184d/USER'
+			'/RPVSt350tojj_13TeV_pythia8/algomez-MiniAOD_PHYS14_v720_PU20bx25-b1b44dbfc276814daa37c582f825184d/USER'
+			]
+	
+	for dataset in QCDHT:
+		config.Data.inputDataset = dataset
+		config.General.requestName = dataset.replace('/','_')+'_RUNtuples_'+version
+		crabCommand('submit', config = config)
--- RUNtuples/test/crab3_QCD.py
+++ RUNtuples/test/crab3_QCD.py
@@ -1,6 +1,13 @@
+##################################################################
+########   TO RUN THIS: python crab3_QCD.py
+########   DO NOT DO: crab submit crab3_QCD.py
+##################################################################
+
 from CRABClient.UserUtilities import config
 config = config()
 
+
+version = 'v03'
 
 config.General.requestName = ''
 config.General.workArea = 'crab_projects'
@@ -10,12 +17,12 @@ config.JobType.psetName = 'RUNtuples_cfg.py'
 config.JobType.allowNonProductionCMSSW = True
 
 config.Data.inputDataset = ''
-config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 1
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 20
 config.Data.outLFN = '/store/user/algomez/'
 config.Data.publication = True
 config.Data.ignoreLocality = True
-config.Data.publishDataName = 'RUNA_PHYS14_PU20bx25_v01'
+config.Data.publishDataName = 'RUNA_PHYS14_PU20bx25_'+version
 
 config.Site.storageSite = 'T3_US_FNALLPC'
 
@@ -23,12 +30,12 @@ if __name__ == '__main__':
 
 	from CRABAPI.RawCommand import crabCommand
 
-	QCDHT = [ '/QCD_HT-100To250_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM',
-			'/QCD_HT_250To500_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM',
+	QCDHT = [ #'/QCD_HT-100To250_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM',
+			#'/QCD_HT_250To500_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM',
 			'/QCD_HT-500To1000_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM',
-			'/QCD_HT-500To1000_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1_ext1-v1/MINIAODSIM',
-			'/QCD_HT_1000ToInf_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM',
-			'/QCD_HT_1000ToInf_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1_ext1-v1/MINIAODSIM'
+			#'/QCD_HT-500To1000_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1_ext1-v1/MINIAODSIM',
+			#'/QCD_HT_1000ToInf_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM',
+			#'/QCD_HT_1000ToInf_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1_ext1-v1/MINIAODSIM'
 			]
 
 	### CSA14 samples
@@ -43,5 +50,5 @@ if __name__ == '__main__':
 	
 	for dataset in QCDHT:
 		config.Data.inputDataset = dataset
-		config.General.requestName = dataset.replace('/','_')+'_RUNtuples_v01'
+		config.General.requestName = dataset.replace('/','_')+'_RUNtuples_'+version
 		crabCommand('submit', config = config)
