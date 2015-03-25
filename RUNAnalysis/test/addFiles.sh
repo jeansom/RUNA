f59c28ca4f979de8812f507f507c1ba8c88a8ff4
Alejandro <gomez@rutgers.physics.edu>
2015-03-24 22:42:48 -0500

    adding an example code

--- RUNAnalysis/test/addFiles.sh
+++ RUNAnalysis/test/addFiles.sh
@@ -1,11 +1,19 @@
-hadd anaPlots_QCD_Pt80to120_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-80to120_Tune4C_13TeV_pythia8/crab_QCD_Pt-80to120_Tune4C_13TeV_pythia8_RUNPlots_PU40bx50_v0/*/*/*root
-hadd anaPlots_QCD_Pt120to170_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-120to170_Tune4C_13TeV_pythia8/crab_QCD_Pt-120to170_Tune4C_13TeV_pythia8_RUNPlots_PU40bx50_v0/*/*/*root
-hadd anaPlots_QCD_Pt170to300_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-170to300_Tune4C_13TeV_pythia8/crab_QCD_Pt-170to300_Tune4C_13TeV_pythia8_RUNPlots_PU40bx50_v0/*/*/*root
-hadd anaPlots_QCD_Pt300to470_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-300to470_Tune4C_13TeV_pythia8/crab_QCD_Pt-300to470_Tune4C_13TeV_pythia8_RUNPlots_PU40bx50_v0/*/*/*root
-hadd anaPlots_QCD_Pt470to600_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-470to600_Tune4C_13TeV_pythia8/crab_QCD_Pt-470to600_Tune4C_13TeV_pythia8_RUNPlots_PU40bx50_v0/*/*/*root
-hadd anaPlots_QCD_Pt600to800_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-600to800_Tune4C_13TeV_pythia8/crab_QCD_Pt-600to800_Tune4C_13TeV_pythia8_RUNPlots_PU40bx50_v0/*/*/*root
-hadd anaPlots_QCD_Pt800to1000_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-800to1000_Tune4C_13TeV_pythia8/crab_QCD_Pt-800to1000_Tune4C_13TeV_pythia8_RUNPlots_PU40bx50_v0/*/*/*root
+#hadd RUNAPlots_QCD_Pt80to120_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-80to120_Tune4C_13TeV_pythia8/crab_QCD_Pt-80to120_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
+#hadd RUNAPlots_QCD_Pt120to170_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-120to170_Tune4C_13TeV_pythia8/crab_QCD_Pt-120to170_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
+#hadd RUNAPlots_QCD_Pt170to300_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-170to300_Tune4C_13TeV_pythia8/crab_QCD_Pt-170to300_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
+#hadd RUNAPlots_QCD_Pt300to470_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-300to470_Tune4C_13TeV_pythia8/crab_QCD_Pt-300to470_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
+#hadd RUNAPlots_QCD_Pt470to600_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-470to600_Tune4C_13TeV_pythia8/crab_QCD_Pt-470to600_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
+#hadd RUNAPlots_QCD_Pt600to800_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-600to800_Tune4C_13TeV_pythia8/crab_QCD_Pt-600to800_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
+#hadd RUNAPlots_QCD_Pt800to1000_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-800to1000_Tune4C_13TeV_pythia8/crab_QCD_Pt-800to1000_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
+##
+#hadd RUNAPlots_QCDALL_PU40bx50.root anaPlots_QCD*PU40bx50.root
+##
+#hadd RUNAPlots_RPVSt100tojj_PU40bx50.root /eos/uscms/store/user/algomez/RPVSt100tojj_13TeV_pythia8_GENSIM/crab_RPVSt100tojj_13TeV_pythia8_GENSIM_PU40bx50_v01v02/*/*root
 
-hadd anaPlots_QCDALL_PU20bx25.root anaPlots_QCD*
 
-#hadd anaPlots_RPVSt100tojj_PU20bx25.root /eos/uscms/store/user/algomez/RPVSt100tojj_13TeV_pythia8_GENSIM/anaPlots_v0_RPVSt100tojj_13TeV_pythia8_20bx25_anaPlots/*/*root
+hadd RUNAPlots_RPVSt100tojj_PU20bx25.root ~/EOS/RPVSt100tojj_13TeV_pythia8/crab_RPVSt100tojj_13TeV_pythia8_*/*/*/*root
+#hadd RUNAPlots_RPVSt100tobj_PU20bx25.root ~/EOS/RPVSt100tobj_13TeV_pythia8/crab__RPVSt100tobj_13TeV_pythia8_*/*/*/*root
+hadd RUNAPlots_QCD_HT-500To1000_PU20bx25.root  ~/EOS/QCD_HT-500To1000_13TeV-madgraph/crab_QCD_HT-500To1000_13TeV-madgraph*/*/*/*.root 
+hadd RUNAPlots_QCD_HT-1000ToInf_PU20bx25.root ~/EOS/QCD_HT_1000ToInf_13TeV-madgraph/crab_QCD_HT_1000ToInf_13TeV-madgraph*/*/*/*.root
+hadd RUNAPlots_QCDALL_PU20bx25.root RUNAPlots_QCD_HT-*
+
