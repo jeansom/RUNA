dd98add3d933a2b7a111419bbd4e06ec5fd04815
Alejandro <gomez@rutgers.physics.edu>
2015-04-17 07:09:03 -0500

    adding more functionalities to addFiles

--- RUNAnalysis/test/addFiles.sh
+++ RUNAnalysis/test/addFiles.sh
@@ -1,27 +1,40 @@
-#hadd RUNAnalysis_QCD_Pt80to120_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-80to120_Tune4C_13TeV_pythia8/crab_QCD_Pt-80to120_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
-#hadd RUNAnalysis_QCD_Pt120to170_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-120to170_Tune4C_13TeV_pythia8/crab_QCD_Pt-120to170_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
-#hadd RUNAnalysis_QCD_Pt170to300_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-170to300_Tune4C_13TeV_pythia8/crab_QCD_Pt-170to300_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
-#hadd RUNAnalysis_QCD_Pt300to470_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-300to470_Tune4C_13TeV_pythia8/crab_QCD_Pt-300to470_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
-#hadd RUNAnalysis_QCD_Pt470to600_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-470to600_Tune4C_13TeV_pythia8/crab_QCD_Pt-470to600_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
-#hadd RUNAnalysis_QCD_Pt600to800_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-600to800_Tune4C_13TeV_pythia8/crab_QCD_Pt-600to800_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
-#hadd RUNAnalysis_QCD_Pt800to1000_PU40bx50.root /eos/uscms/store/user/algomez/QCD_Pt-800to1000_Tune4C_13TeV_pythia8/crab_QCD_Pt-800to1000_Tune4C_13TeV_pythia8_PU40bx50_v01v02/*/*/*root
-##
-#hadd RUNAnalysis_QCDALL_PU40bx50.root anaPlots_QCD*PU40bx50.root
-##
-#hadd RUNAnalysis_RPVSt100tojj_PU40bx50.root /eos/uscms/store/user/algomez/RPVSt100tojj_13TeV_pythia8_GENSIM/crab_RPVSt100tojj_13TeV_pythia8_GENSIM_PU40bx50_v01v02/*/*root
+version="v03_v06"
+PUList="PU40bx50 PU20bx25"
+ptBinList="Pt-170to300 Pt-300to470 Pt-470to600 Pt-600to800 Pt-800to1000 Pt-1000to1400 Pt-1400to1800"  
+htBinList="HT-500To1000 HT_1000ToInf"
+campaignList="CSA14 PHYS14"
+PU=$2
+camp=$3
 
+#for PU in $PUList
+#do
+#	for camp in $campaignList
+#	do
+if [[ $1 == *"QCD"* ]]; then
 
-#hadd RUNAnalysis_RPVSt100tojj_PU20bx25.root ~/EOS/RPVSt100tojj_13TeV_pythia8/crab_RPVSt100tojj_13TeV_pythia8_*V04/*/*/*root
-##hadd RUNAnalysis_RPVSt100tobj_PU20bx25.root ~/EOS/RPVSt100tobj_13TeV_pythia8/crab__RPVSt100tobj_13TeV_pythia8_*/*/*/*root
-#hadd RUNAnalysis_QCD_HT-500To1000_PU20bx25.root  ~/EOS/QCD_HT-500To1000_13TeV-madgraph/crab_QCD_HT-500To1000_13TeV-madgraph*V04/*/*/*.root 
-#hadd RUNAnalysis_QCD_HT-1000ToInf_PU20bx25.root ~/EOS/QCD_HT_1000ToInf_13TeV-madgraph/crab_QCD_HT_1000ToInf_13TeV-madgraph*V04/*/*/*.root
-#hadd RUNAnalysis_QCDALL_PU20bx25.root RUNAnalysis_QCD_HT-*
+	if [[ $1 == *"Pt"* ]]; then
+		for bin in $ptBinList
+		do
+			rm -rf RUNAnalysis_QCD_${bin}_${camp}_${PU}_${version}.root 
+			hadd RUNAnalysis_QCD_${bin}_${camp}_${PU}_${version}.root /eos/uscms/store/user/algomez/QCD_${bin}_Tune4C_13TeV_pythia8/crab_QCD_${bin}_Tune4C_13TeV_pythia8_${camp}_${PU}_${version}/*/*/*root
+		done
+		rm -rf RUNAnalysis_QCDPtAll_${camp}_${PU}_${version}.root 
+		hadd RUNAnalysis_QCDPtAll_${camp}_${PU}_${version}.root RUNAnalysis_QCD_Pt*_${camp}_${PU}_${version}.root
+	else
+		for bin in $htBinList
+		do
+			rm -rf RUNAnalysis_QCD_${bin}_${camp}_${PU}_${version}.root 
+			hadd RUNAnalysis_QCD_${bin}_${camp}_${PU}_${version}.root /eos/uscms/store/user/algomez/QCD_${bin}_13TeV-madgraph/crab_QCD_${bin}_13TeV-madgraph_${camp}_${PU}_${version}/*/*/*root
+		done
+		rm -rf RUNAnalysis_QCDHTAll_${camp}_${PU}_${version}.root 
+		hadd RUNAnalysis_QCDHTAll_${camp}_${PU}_${version}.root RUNAnalysis_QCD_HT*_${camp}_${PU}_${version}.root
+	fi
 
-hadd RUNAnalysis_QCD_Pt170to300_PHYS14_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-170to300_Tune4C_13TeV_pythia8/crab_QCD_Pt-170to300_Tune4C_13TeV_pythia8_PHYS14_PU20bx25_v03_v04/*/*/*root
-hadd RUNAnalysis_QCD_Pt300to470_PHYS14_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-300to470_Tune4C_13TeV_pythia8/crab_QCD_Pt-300to470_Tune4C_13TeV_pythia8_PHYS14_PU20bx25_v03_v04/*/*/*root
-hadd RUNAnalysis_QCD_Pt470to600_PHYS14_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-470to600_Tune4C_13TeV_pythia8/crab_QCD_Pt-470to600_Tune4C_13TeV_pythia8_PHYS14_PU20bx25_v03_v04/*/*/*root
-hadd RUNAnalysis_QCD_Pt600to800_PHYS14_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-600to800_Tune4C_13TeV_pythia8/crab_QCD_Pt-600to800_Tune4C_13TeV_pythia8_PHYS14_PU20bx25_v03_v04/*/*/*root
-hadd RUNAnalysis_QCD_Pt800to1000_PHYS14_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-800to1000_Tune4C_13TeV_pythia8/crab_QCD_Pt-800to1000_Tune4C_13TeV_pythia8_PHYS14_PU20bx25_v03_v04/*/*/*root
-hadd RUNAnalysis_QCD_Pt1000to1400_PHYS14_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-1000to1400_Tune4C_13TeV_pythia8/crab_QCD_Pt-1000to1400_Tune4C_13TeV_pythia8_PHYS14_PU20bx25_v03_v04/*/*/*root
-hadd RUNAnalysis_QCD_Pt1400to1800_PHYS14_PU20bx25.root /eos/uscms/store/user/algomez/QCD_Pt-1400to1800_Tune4C_13TeV_pythia8/crab_QCD_Pt-1400to1800_Tune4C_13TeV_pythia8_PHYS14_PU20bx25_v03_v04/*/*/*root
-hadd RUNAnalysis_QCDALL_PU20bx25.root RUNAnalysis_QCD*_PHYS14_PU20bx25.root
+else
+	rm -rf RUNAnalysis_RPVSt100tojj_${camp}_${PU}_${version}.root 
+	hadd RUNAnalysis_RPVSt100tojj_GENSIM_${camp}_${PU}_${version}.root /eos/uscms/store/user/algomez/RPVSt100tojj_13TeV_pythia8_GENSIM/crab_RPVSt100tojj_13TeV_pythia8_*${version}/*/*/*root
+	#rm -rf RUNAnalysis_RPVSt100tojj_${camp}_${PU}_${version}.root 
+	#hadd RUNAnalysis_RPVSt100tojj_${camp}_${PU}_${version}.root /eos/uscms/store/user/algomez/RPVSt100tojj_13TeV_pythia8/crab_RPVSt100tojj_13TeV_pythia8_*${version}/*/*/*root
+fi
+#	done
+#done 
