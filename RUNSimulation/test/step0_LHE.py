be1f1566ce44df14b0d2d52b7378c135859a6e58
Alejandro <gomez@physics.rutgers.edu>
2015-07-15 03:58:47 +0200

    adding step0 for gridpacks

---
+++ RUNSimulation/test/step0_LHE.py
@@ -0,0 +1,83 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/B2G-RunIIWinter15wmLHE-00025-fragment.py --fileout file:B2G-RunIIWinter15wmLHE-00025.root --mc --eventcontent LHE --datatier LHE --conditions MCRUN2_71_V1::All --step LHE --python_filename /afs/cern.ch/cms/PPD/PdmV/work/McM/submit/B2G-RunIIWinter15wmLHE-00025/B2G-RunIIWinter15wmLHE-00025_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 703
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('LHE')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.19 $'),
+    annotation = cms.untracked.string('Configuration/GenProduction/python/B2G-RunIIWinter15wmLHE-00025-fragment.py nevts:703'),
+    name = cms.untracked.string('Applications')
+)
+
+# Output definition
+
+process.LHEoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.LHEEventContent.outputCommands,
+    fileName = cms.untracked.string('file:B2G-RunIIWinter15wmLHE-00025.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('LHE')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'MCRUN2_71_V1::All', '')
+
+process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    nEvents = cms.untracked.uint32(703),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh'),
+    numberOfParameters = cms.uint32(1),
+    args = cms.vstring('/afs/cern.ch/work/a/algomez/Substructure/CMSSW_7_4_1_patch1/src/RUNA/RUNSimulation/test/RPVSt100_UDD312_ISR2j_LO_gridpack.tar.gz')
+)
+
+
+# Path and EndPath definitions
+process.lhe_step = cms.Path(process.externalLHEProducer)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.LHEoutput_step = cms.EndPath(process.LHEoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.lhe_step,process.endjob_step,process.LHEoutput_step)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
