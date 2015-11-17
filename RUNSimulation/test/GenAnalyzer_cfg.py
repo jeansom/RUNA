1ed1f13e9a39e2d9412a067d05462ef0c0cedbeb
Alejandro <gomez@physics.rutgers.edu>
2015-11-18 00:46:01 +0100

    adding GenAnalyzer files

--- RUNSimulation/test/GenAnalyzer_cfg.py
+++ RUNSimulation/test/GenAnalyzer_cfg.py
@@ -3,29 +3,25 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.load("RPVSt100tojj_13TeV_pythia8_GENSIM_TESTHT500_cfi")
+#process.load("RPVSt100tojj_13TeV_pythia8_GENSIM_TESTHT500_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
-'''
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
         'file:RPVSt200tojj_13TeV_AODSIM_test.root'
     )
 )
-'''
 
 process.TFileService = cms.Service("TFileService",
 		fileName = cms.string('GenAnalyzer.root')
 )
 
-process.demo = cms.EDAnalyzer('GenAnalyzer',
-		src = cms.InputTag( 'GenParticles' ),
-		stop1Mass = cms.double( 100 ),
-		stop2Mass = cms.double( 100 ),
-		st1decay = cms.double( 1 ),
+process.genPlots = cms.EDAnalyzer('GenAnalyzer',
+		genAK4Jets	= cms.InputTag('ak4GenJetsNoNu'),
+		genAK8Jets	= cms.InputTag('ak8GenJetsNoNu'),
 )
 
 
-process.p = cms.Path(process.demo)
+process.p = cms.Path(process.genPlots)
