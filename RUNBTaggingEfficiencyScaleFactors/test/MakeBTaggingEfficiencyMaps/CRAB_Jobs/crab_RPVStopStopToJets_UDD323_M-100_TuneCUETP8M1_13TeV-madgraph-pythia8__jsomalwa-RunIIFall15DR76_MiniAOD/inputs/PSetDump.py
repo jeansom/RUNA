49e3de8b4c4b2a5e8a0b533c24bba54b0b5bf75b
Jean Som <jsomalwar@gmail.com>
2016-08-01 15:51:21 -0500

    Fixed

---
+++ RUNBTaggingEfficiencyScaleFactors/test/MakeBTaggingEfficiencyMaps/CRAB_Jobs/crab_RPVStopStopToJets_UDD323_M-100_TuneCUETP8M1_13TeV-madgraph-pythia8__jsomalwa-RunIIFall15DR76_MiniAOD/inputs/PSetDump.py
@@ -0,0 +1,132 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("USER")
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('root://cmseos.fnal.gov//store/user/jsomalwa/RPVStopStopToJets_UDD323_M-100_TuneCUETP8M1_13TeV-madgraph-pythia8/RunIIFall15DR76_MiniAOD_Asympt25ns/160202_114630/0000/RPVStopStopToJets_UDD323_M-100-madgraph_MiniAOD_Asympt25ns_986.root')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+)
+
+process.bTaggingEffAnalyzerAK8PF = cms.EDAnalyzer("BTaggingEffAnalyzer",
+    DiscriminatorTag = cms.string('pfCombinedInclusiveSecondaryVertexV2BJetTags'),
+    DiscriminatorValue = cms.double(0.8),
+    EtaMax = cms.double(3.0),
+    EtaMin = cms.double(-3.0),
+    EtaNBins = cms.int32(60),
+    JetCSVTag = cms.InputTag(""),
+    JetEtaTag = cms.InputTag(""),
+    JetMassTag = cms.InputTag(""),
+    JetPartonFlavorTag = cms.InputTag(""),
+    JetPhiTag = cms.InputTag(""),
+    JetPtTag = cms.InputTag(""),
+    JetsTag = cms.InputTag("slimmedJets"),
+    PtMax = cms.double(1000.0),
+    PtMin = cms.double(0.0),
+    PtNBins = cms.int32(100),
+    isMiniAOD = cms.int32(0),
+    rho = cms.InputTag("fixedGridRhoFastjetCentralNeutral"),
+    vertices = cms.InputTag("offlineSlimmedPrimaryVertices")
+)
+
+
+process.p = cms.Path(process.bTaggingEffAnalyzerAK8PF)
+
+
+process.MessageLogger = cms.Service("MessageLogger",
+    FrameworkJobReport = cms.untracked.PSet(
+        FwkJob = cms.untracked.PSet(
+            limit = cms.untracked.int32(10000000),
+            optionalPSet = cms.untracked.bool(True)
+        ),
+        default = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        optionalPSet = cms.untracked.bool(True)
+    ),
+    categories = cms.untracked.vstring('FwkJob', 
+        'FwkReport', 
+        'FwkSummary', 
+        'Root_NoDictionary'),
+    cerr = cms.untracked.PSet(
+        FwkJob = cms.untracked.PSet(
+            limit = cms.untracked.int32(0),
+            optionalPSet = cms.untracked.bool(True)
+        ),
+        FwkReport = cms.untracked.PSet(
+            limit = cms.untracked.int32(10000000),
+            optionalPSet = cms.untracked.bool(True),
+            reportEvery = cms.untracked.int32(1000)
+        ),
+        FwkSummary = cms.untracked.PSet(
+            limit = cms.untracked.int32(10000000),
+            optionalPSet = cms.untracked.bool(True),
+            reportEvery = cms.untracked.int32(1)
+        ),
+        INFO = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        Root_NoDictionary = cms.untracked.PSet(
+            limit = cms.untracked.int32(0),
+            optionalPSet = cms.untracked.bool(True)
+        ),
+        default = cms.untracked.PSet(
+            limit = cms.untracked.int32(10000000)
+        ),
+        noTimeStamps = cms.untracked.bool(False),
+        optionalPSet = cms.untracked.bool(True),
+        threshold = cms.untracked.string('INFO')
+    ),
+    cerr_stats = cms.untracked.PSet(
+        optionalPSet = cms.untracked.bool(True),
+        output = cms.untracked.string('cerr'),
+        threshold = cms.untracked.string('WARNING')
+    ),
+    cout = cms.untracked.PSet(
+        placeholder = cms.untracked.bool(True)
+    ),
+    debugModules = cms.untracked.vstring(),
+    debugs = cms.untracked.PSet(
+        placeholder = cms.untracked.bool(True)
+    ),
+    default = cms.untracked.PSet(
+
+    ),
+    destinations = cms.untracked.vstring('warnings', 
+        'errors', 
+        'infos', 
+        'debugs', 
+        'cout', 
+        'cerr'),
+    errors = cms.untracked.PSet(
+        placeholder = cms.untracked.bool(True)
+    ),
+    fwkJobReports = cms.untracked.vstring('FrameworkJobReport'),
+    infos = cms.untracked.PSet(
+        Root_NoDictionary = cms.untracked.PSet(
+            limit = cms.untracked.int32(0),
+            optionalPSet = cms.untracked.bool(True)
+        ),
+        optionalPSet = cms.untracked.bool(True),
+        placeholder = cms.untracked.bool(True)
+    ),
+    statistics = cms.untracked.vstring('cerr_stats'),
+    suppressDebug = cms.untracked.vstring(),
+    suppressInfo = cms.untracked.vstring(),
+    suppressWarning = cms.untracked.vstring(),
+    warnings = cms.untracked.PSet(
+        placeholder = cms.untracked.bool(True)
+    )
+)
+
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('bTaggingEfficiency.root')
+)
+
+
